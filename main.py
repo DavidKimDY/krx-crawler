c40e8407d72648f11c6dabeb7138df3aba918863
Ubuntu <ubuntu@ip-172-31-32-152.ap-northeast-2.compute.internal>
2021-07-05 11:08:34 +0900

    Edit : simplized

--- main.py
+++ main.py
@@ -2,24 +2,33 @@ import datetime
 import os
 from time import sleep
 
-now = datetime.datetime.now()
-time = now.time()
-weekday = now.weekday()
-
-A_HOUR = 60 * 60
-A_DAY = A_HOUR * 24
-
-print(time)
-print(weekday)
-
 os.system('python3 timezone.py')
 
-if weekday < 5:
-    if time > datetime.time(15,30):
-        print('sh run4.bash')
-        os.system('sh run4.bash')
-        sleep(A_DAY)
+while True:
+    now = datetime.datetime.now()
+    time = now.time()
+    weekday = now.weekday()
+
+    A_HOUR = 60 * 60
+    FIVE_MIN = 60 * 5
+    A_DAY = A_HOUR * 24
+
+    print('*'*30, str(now), '*'*30)
+    print(time)
+    print(weekday)
+
+    with open('log/' +str(time) + '.txt', 'w') as f: 
+        f.write('')
+    
+    if weekday < 5:
+        if time > datetime.time(15,30):
+            with open('log/running.txt', 'w') as f:
+                f.write('runing')
+            print('sh run4.bash')
+            os.system('sh run4.bash')
+            sleep(A_DAY)
+        else:
+            sleep(FIVE_MIN)
     else:
-        sleep(A_HOUR)
-else:
-    sleep(A_DAY)
+        sleep(A_DAY)
+
